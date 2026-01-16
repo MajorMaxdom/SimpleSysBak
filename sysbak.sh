e412f87d58bd11da58b5caae6a89159774e60bbd
Janik Ludewig <janik.ludewig@googlemail.com>
2026-01-16 17:30:17 +0100

    modified:   sysbak.sh

--- sysbak.sh
+++ sysbak.sh
@@ -8,18 +8,25 @@ CONFIG_FILE="$BASE_DIR/simplesysbak_config.json"
 RUN_PREFIX="M-"
 [ "$1" = "-automatic" ] && RUN_PREFIX="A-"
 
-export CONFIG_FILE RUN_PREFIX
+# Config MUSS existieren
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Config fehlt: $CONFIG_FILE"
+  echo "Bitte zuerst das Install-Skript ausführen."
+  exit 1
+fi
 
+# Module laden
 source "$LIB_DIR/mount.sh"
 source "$LIB_DIR/backup.sh"
 source "$LIB_DIR/retention.sh"
 
 cleanup() {
-  unmount_share
+  unmount_share "$CONFIG_FILE"
   cleanup_tmp
 }
 trap cleanup EXIT
 
-mount_share
-run_backup
-apply_retention
+# Ablauf
+mount_share "$CONFIG_FILE"
+run_backup "$CONFIG_FILE" "$RUN_PREFIX"
+apply_retention "$CONFIG_FILE"
