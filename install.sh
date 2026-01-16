7301ee40739ad290e419408d54ca4c2c30596b24
Janik Ludewig <janik.ludewig@googlemail.com>
2026-01-16 16:33:23 +0100

    new: created readme

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 cat <<'EOF'
-   _____            ____        __  
-  / ___/__  _______/ __ )____ _/ /__
-  \__ \/ / / / ___/ __  / __ `/ //_/
- ___/ / /_/ (__  ) /_/ / /_/ / ,<   
-/____/\__, /____/_____/\__,_/_/|_|  
-     /____/                         
+   _____ _                 __    _____            ____        __  
+  / ___/(_)___ ___  ____  / /__ / ___/__  _______/ __ )____ _/ /__
+  \__ \/ / __ `__ \/ __ \/ / _ \\__ \/ / / / ___/ __  / __ `/ //_/
+ ___/ / / / / / / / /_/ / /  __/__/ / /_/ (__  ) /_/ / /_/ / ,<   
+/____/_/_/ /_/ /_/ .___/_/\___/____/\__, /____/_____/\__,_/_/|_|  
+                /_/                /____/                         
 EOF
 
+echo "Installing SimpleSysBak"
+
+echo "Creating /usr/local/systembackup directory"
 mkdir -p /usr/local/systembackup/lib
+
+echo "Copying all files"
 cp systembackup.sh /usr/local/systembackup/
 cp lib/*.sh /usr/local/systembackup/lib/
+
+echo "Making systembackup.sh executable"
 chmod +x /usr/local/systembackup/systembackup.sh
+
+echo "All Done. Have fun backing up!"
